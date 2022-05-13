7c19b93a9acdb35a64b86644f09dbae862e2c5af
mini-cacao <mingcao54@gmail.com>
2022-05-13 13:07:10 -0600

    Add a test_data csv, and use here::here() to open its file path

--- testRscript_20220512.R
+++ testRscript_20220512.R
@@ -5,4 +5,7 @@ library(here)
 
 print("hello May")
 
-here::here() # returns the path in string
+here::here() # this generates the path in string
+
+my_data <- read.delim(here::here("test_data.csv")) # call a csv file inside this R project
+my_data
